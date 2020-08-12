fe6883df8557fc3e290e8eca58e9b118799b18f9
Tautvydas Dulskis <tautvydas.dulskis@hostinger.com>
2020-08-12 21:21:03 +0300

    Composer klases darbas

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,29 @@
 <?php
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::DEBUG));
+
+function tikrintiSkaiciu($skaicius) {
+    if (!is_numeric($skaicius)){
+        throw new Exception('Perduotas parametras nera skaicius');
+    }
+    if($skaicius > 1) {
+        throw new Exception('Skaičius privalo būti mažesnis už 1');
+    }
+}
+
+try {
+    $skaicius = $_GET['nr'] ? (int) $_GET['nr'] : 0;
+    tikrintiSkaiciu($skaicius);
+    echo 'Jei matote šį pranešimą, skaičius yra mažesnis už vienetą';
+
+} catch(Exception $exception) {
+    echo 'Atsiprasome nutiko klaida';
+    $log->error('Skaiciau tikrinimo klaida', ['message' => $exception->getMessage()]);
+}
+
+$obj = new \KCS\ObjektoKlase();
+
+
---
+++ src/ObjektoKlase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KCS;
+
+class ObjektoKlase
+{
+    public function __construct()
+    {
+        echo "Labas pasauli";
+    }
+}
