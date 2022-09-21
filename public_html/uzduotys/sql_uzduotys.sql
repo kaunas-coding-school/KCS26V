d71b87378c27c987845c24c632b8224714e648d7
Tautvydas Dulskis <807294+tautiz@users.noreply.github.com>
2022-09-21 21:38:06 +0300

    Update sql_uzduotys.sql

--- public_html/uzduotys/sql_uzduotys.sql
+++ public_html/uzduotys/sql_uzduotys.sql
@@ -1,23 +1,23 @@
-# 1] Uzduotis
+-- 1] Uzduotis
 SELECT count(u.id) AS kiekis FROM users as u
         LEFT JOIN states as s
         ON s.id = u.state
 where s.title = 'Suspended';
 
-# 2] Kiek yra aktyviu grupiu
+-- 2] Kiek yra aktyviu grupiu
 SELECT count(g.id) AS kiekis
 FROM states as s
         RIGHT JOIN gruops as g
         ON g.state = s.id
 where s.title = 'Active';
 
-# 3] Personu vardai pavardes ir kuriuose miestuose ar sal;yse jie gyvena
+-- 3] Personu vardai pavardes ir kuriuose miestuose ar sal;yse jie gyvena
 SELECT p.first_name, p.last_name, a.city, c.title
 FROM persons as p
          LEFT JOIN address as a on a.id = p.address_id
          LEFT JOIN countries as c on c.iso = a.country_iso;
 
-# 4] uzduotis
+-- 4] uzduotis
 SELECT g.code, COUNT(p.id) AS kiekis
 FROM gruops as g
          LEFT JOIN states AS s ON g.state = s.id
@@ -26,21 +26,21 @@ FROM gruops as g
 WHERE s.title = 'Active'
 GROUP BY g.`code`;
 
-# 5a] uzduotis
+-- 5a] uzduotis
 SELECT p.*, g.code
 FROM gruops as g
          LEFT JOIN person2gruop AS p2g ON p2g.gruop_id = g.id
          LEFT JOIN persons AS p ON p.id = p2g.person_id
 WHERE g.code LIKE '%D';
 
-# 5b] uzduotis
+--# 5b] uzduoti
 SELECT COUNT(p.id) as kiekis
 FROM gruops as g
          LEFT JOIN person2gruop AS p2g ON p2g.gruop_id = g.id
          LEFT JOIN persons AS p ON p.id = p2g.person_id
 WHERE g.code LIKE '%D';
 
-#6] uzduotis
+-- 6] uzduotis
 SELECT p.first_name, p.last_name, CONCAT(a.street, ' ', a.city, ' ',  a.postcode, ' ', c.title) AS Adresas
 FROM `groups` g
          JOIN person2gruop p2g on g.id = p2g.groups_id
@@ -48,6 +48,28 @@ FROM `groups` g
          JOIN addresses a on p.address_id = a.id
          JOIN countries c on a.country_iso = c.iso
 where g.title = 'KCS511v';
+
+-- 7] uzduotis JOIN
+SELECT p.*, s.title
+FROM persons p
+         JOIN users u on p.id = u.person_id
+         JOIN states s on u.state_id = s.id
+where s.title = 'Inactive'
+AND (p.first_name is null or p.first_name = '' OR p.last_name is null OR p.last_name = '')
+GROUP BY p.id;
+
+-- 8] uzduotis JOIN
+SELECT count(g.id) AS `Kiek grupiu adrese`,
+       concat(
+               COALESCE(a.street, ''), ' ',
+               COALESCE(a.city, ''), ' ',
+               COALESCE(a.postcode, ''), ' ',
+               COALESCE(c.title, '')
+           ) AS Adresas
+FROM `groups` g
+         JOIN addresses a on g.address_id = a.id
+         LEFT JOIN countries c on a.country_iso = c.iso
+GROUP BY g.address_id;
 
 # *a] uzduotis
 SELECT COUNT(p.id) as kiekis,
